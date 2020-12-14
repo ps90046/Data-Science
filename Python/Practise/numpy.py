14aa5225d90bf5dbc467533754a7330209331aab
ps90046 <72904409+ps90046@users.noreply.github.com>
2020-12-14 10:14:45 +0530

    Delete numpy.py

--- Python/Practise/numpy.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-a = np.random.randn(100)
-N = 1000
-idx = np.random.randint(0,a.size,(N,a.size))
-means = a[idx].mean(axis=1)
-confint = np.percentile(means, [2.5,7.5])
-print(confint)
